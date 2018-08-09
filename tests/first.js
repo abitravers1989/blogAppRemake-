d8f0f6fe7fbf9eaed7ac66821cdf361c83954878
abitravers1989 <atravers4007@gmail.com>
2018-08-09 10:34:49 +0100

    Adding tests

---
+++ tests/first.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+const mongoose = require('mongoose');
+require("sinon-mongoose");
+
+describe("A post is saved successfully to the database", function () {
+    it("should create a new post", function (done) {
+
+    })
+})
+
+
+https://semaphoreci.com/community/tutorials/a-tdd-approach-to-building-a-todo-api-using-node-js-and-mongodb
